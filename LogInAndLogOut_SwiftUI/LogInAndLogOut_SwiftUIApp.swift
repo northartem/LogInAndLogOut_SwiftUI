82ed5b8b75323937f1a0a1560ce86fdada6e8b30
AssanCred <temix@live.ru>
2023-06-16 16:17:31 +0700

    Initial Commit

---
+++ LogInAndLogOut_SwiftUI/LogInAndLogOut_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInAndLogOut_SwiftUIApp.swift
+//  LogInAndLogOut_SwiftUI
+//
+//  Created by Артём Латушкин on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct LogInAndLogOut_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
